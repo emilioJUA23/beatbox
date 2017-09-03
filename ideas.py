f9ceb07f75e92a8d62afc8cab39a9954d5a7c586
Luis Emilio <luis.juarez2394gmail.com>
2017-09-03 18:53:11 +0000

    correciones

--- ideas.py
+++ ideas.py
@@ -1,18 +1,25 @@
 from gpiozero import LED
+from gpiozero import Button
 from time import sleep
 import pygame
 
-led1 = None  #led de prueba para saludo y repite el patron
-metronomo(None)
+pygame.init()
+pygame.mixer.music.load('metro_bar.wav')
+led1 = LED(27)  #led de prueba para saludo y repite el patron
+metronomo=None
 nivel = 3
-tempo = 110
+tempo = 60.0
 
 #funcion para apagar la raspberry
 def shutdown():
     check_call(['sudo', 'poweroff'])
 
+def playSound(filename):
+    #pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+
 # donde x es el numero de gpio que usaremos en el boton
-shutdown_btn = Button(x, hold_time=2)
+shutdown_btn = Button(2, hold_time=2)
 shutdown_btn.when_held = shutdown
 
 
@@ -33,45 +40,54 @@ def calcular_figuras(bmp):
 def nivel_3(led):
     # primer compas
     #1
+    playSound('metro_bar.wav')
     led.on()
     sleep(f['corchea'])
     #an
     led.off()
     sleep(f['corchea'])
     # 2
-    led.on()
-    sleep(f['corchea'])
-    # an
-    led.on()
-    sleep(f['corchea'])
-    # 3
+    playSound('metro_bar.wav')
     led.on()
     sleep(f['corchea'])
     # an
     led.off()
+    sleep(f['corchea']*0.1)
+    led.on()
+    sleep(f['corchea']*0.9)
+    # 3
+    playSound('metro_bar.wav')
+    led.off()
+    sleep(f['corchea']*0.1)
+    led.on()
+    sleep(f['corchea']*0.9)
+    # an
+    led.off()
     sleep(f['corchea'])
     # 4
+    playSound('metro_bar.wav')
     led.on()
     sleep(f['corchea'])
     # an
+    led.off()
+    sleep(f['corchea']*0.1)
     led.on()
-    sleep(f['corchea'])
+    sleep(f['corchea']*0.9)
 
-
-saludo = 3  # para poder saludar por 3 segundos al usuario
-while saludo > 3:
-    led1.on()
-    sleep(0.5)
-    led1.off()
-    sleep(0.5)
-    saludo -= 1
 
 f = calcular_figuras(tempo)
 led1.off()
-sleep(4*f['negra'])
+playSound('metro_bar.wav')
+sleep(f['negra'])
+playSound('metro_bar.wav')
+sleep(f['negra'])
+playSound('metro_bar.wav')
+sleep(f['negra'])
+playSound('metro_bar.wav')
+sleep(f['negra'])
 # prosigue el programa
 while True:
-    if nivel == 1:
+    if nivel == 3:
         nivel_3(led1)
 
 
