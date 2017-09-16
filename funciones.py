39a896050f49b362ab39be19f18170d73a974068
Luis Emilio <luis.juarez2394gmail.com>
2017-09-16 02:13:14 +0000

    correciiones de ejecucion.

--- funciones.py
+++ funciones.py
@@ -1,9 +1,14 @@
 #import_seccion
+from subprocess import check_call
+from signal import pause
 from gpiozero import LED
 from gpiozero import Button
 from time import sleep
 import pygame
 
+pygame.init()
+pygame.mixer.music.load('metro_bar.wav')
+    
 def calcular_figuras(bmp):
     negra = 60.0/bmp
     blanca = 2*negra
@@ -24,10 +29,6 @@ def shutdown():
 def shutdown_button(x):
     shutdown_btn = Button(x, hold_time=2)
     shutdown_btn.when_held = shutdown
-
-def activar_sonido():
-    pygame.init()
-    pygame.mixer.music.load('metro_bar.wav')
 
 def playSound(filename):
     pygame.mixer.music.play()
--- ideas.py
+++ ideas.py
@@ -3,7 +3,11 @@ from gpiozero import LED
 from gpiozero import Button
 from time import sleep
 import pygame
-from funciones import calcular_figuras,shutdown_button,activar_sonido,playSound
+import funciones
+from funciones import calcular_figuras,shutdown_button,playSound
+
+pygame.init()
+pygame.mixer.music.load('metro_bar.wav')
 
 def nivel_3(led):
     # primer compas
@@ -51,16 +55,18 @@ def nivel_1():
 def despedida():
     pass
 
-led1 = LED(27)  #led de prueba para saludo y repite el patron
+led1 = LED(17)  #led de prueba para saludo y repite el patron
 shutdown_button(2) #define boton de apagado en el gpio
 nivel = 3       #define el nivel que vamos a practicar
 tempo = 60.0    #el tempo en el cual nos vamos a mover
 f = calcular_figuras(tempo)
 # prosigue el programa
 while True:
-    if nivell==1:
+    if nivel==1:
         nivel_1(led1)
     elif nivel ==2:
         nivel_2
-    else nivel == 3:
+    elif nivel == 3:
         nivel_3(led1)
+    else:
+        pass
