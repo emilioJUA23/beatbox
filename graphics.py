c0f04f2c84276e7e1109f16efca3ced99438ef66
Emilio Juarez <ljuarez@velfasa.com>
2017-11-07 11:08:12 -0600

    forma 2 que determina el bmp terminada

--- graphics.py
+++ graphics.py
@@ -5,6 +5,7 @@ except (SystemError, ValueError, ImportError):
 from PIL import *
 import os
 
+#primera forma es de nivel
 root = Tk()  # create a Tk root window
 
 w = 400  # width for the Tk root
@@ -13,6 +14,8 @@ nivel = 0
 def makeSomething(value):
     global nivel
     nivel = value
+    global root
+    root.destroy()
 
 # calculate x and y coordinates for the Tk root window
 x = 10
@@ -41,5 +44,18 @@ label = Label(topFrame,image= img)
 # label.pack(expand=1, fill=BOTH)
 root.mainloop()  # starts the
 print(nivel)
-# root = Tk()
-# root.mainloop()
+#segunda forma verifica bmp
+root = Tk()
+root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+#dividimos el root en izquierda y derecha para label de metronomo izquierda para botones +/-
+bpm = 60
+leftFrame = Frame(root)
+leftFrame.pack(side=LEFT, fill=BOTH, expand=1)
+rightFrame = Frame(root)
+rightFrame.pack(side=RIGHT, fill=BOTH, expand=1)
+button1 = Button(leftFrame, text="+")
+button1.pack(side=TOP, fill=BOTH, expand=1)
+button2 = Button(leftFrame, text="-")
+button2.pack(side=TOP, fill=BOTH, expand=1)
+
+
