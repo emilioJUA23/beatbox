294c5179e4564a02d3722950f0f274059859afc8
emilioJUA23 <luis.juarez2394@gmail.com>
2017-11-06 18:17:20 -0600

    graphics file

---
+++ graphics.py
@@ -0,0 +1,45 @@
+try:
+    from tkinter import *  # python 3
+except (SystemError, ValueError, ImportError):
+    from Tkinter import *  # python 2
+# from PIL import ImageTk, Image
+import os
+
+root = Tk()  # create a Tk root window
+
+w = 400  # width for the Tk root
+h = 267  # height for the Tk root
+nivel = 0
+def makeSomething(value):
+    global nivel
+    nivel = value
+
+# calculate x and y coordinates for the Tk root window
+x = 10
+y = 10
+
+# set the dimensions of the screen
+# and where it is placed
+root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+topFrame = Frame(root)
+topFrame.pack(side=TOP,fill=BOTH,expand=1)
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM,fill=BOTH,expand=1)
+button1 = Button(bottomFrame, text="1",command=lambda *args: makeSomething(1))
+button1.pack(side=LEFT,fill=BOTH,expand=1)
+button2 = Button(bottomFrame, text="2",command=lambda *args: makeSomething(2))
+button2.pack(side=LEFT,fill=BOTH,expand=1)
+button3 = Button(bottomFrame, text="3",command=lambda *args: makeSomething(3))
+button3.pack(side=LEFT,fill=BOTH,expand=1)
+button4 = Button(bottomFrame, text="4",command=lambda *args: makeSomething(4))
+button4.pack(side=LEFT,fill=BOTH,expand=1)
+button5 = Button(bottomFrame, text="5",command=lambda *args: makeSomething(5))
+button5.pack(side=LEFT,fill=BOTH,expand=1)
+# img = ImageTk.PhotoImage(Image.open("duck.gif"))
+# label = Label(topFrame,image=img)
+label = Label(topFrame, text="Select a Level")
+label.pack(expand=1, fill=BOTH)
+root.mainloop()  # starts the
+print(nivel)
+# root = Tk()
+# root.mainloop()
