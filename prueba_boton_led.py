7ed5e09f364617c0e7957c3428708ac83d87deac
emilioJUA23 <luis.juarez2394@gmail.com>
2017-11-11 23:11:12 -0600

    prueba de boton led11

--- prueba_boton_led.py
+++ prueba_boton_led.py
@@ -2,15 +2,3 @@ from gpiozero import Device, LED, Button
 
 button1 = Button(6)
 button2 = Button(5)
-led = None
-
-def say_hello():
-    global button1
-    del button1
-    led=LED(6)
-    led.on()
-
-button2.when_pressed = say_hello
-
-while True:
-    pass
