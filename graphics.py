61e060964fba4539a6a7c9fb540ebf3ba01e0578
emilioJUA23 <luis.juarez2394@gmail.com>
2017-11-12 12:08:06 -0600

    interfaces graficas mejoradas

--- graphics.py
+++ graphics.py
@@ -3,21 +3,24 @@ try:
 except (SystemError, ValueError, ImportError):
     from Tkinter import *  # python 2
 import os
-from gpiozero import LED
-
 #primera forma es de nivel
-root = Tk()  # create a Tk root window
-
-w = 400  # width for the Tk root
-h = 267  # height for the Tk root
-nivel = 0
-bpm = 60
+w = 480  # width for the Tk root
+h = 290  # height for the Tk root
+nivel = 1
+bpm = 100
 def makeSomething(value=None, value_bmp=None):
     if value:
         global nivel
-        nivel = value
-        global root
-        root.destroy()
+        if value == "+":
+            nivel += 1
+        elif value == "-":
+            nivel -= 1
+        elif value == "des":
+            root.destroy()
+        if nivel > 5:
+            nivel = nivel % 5
+        elif nivel < 1:
+            nivel += 5
     if value_bmp:
         global bpm
         if value_bmp == "+":
@@ -47,51 +50,87 @@ def bpm_definition(value_bmp,win):
 x = 10
 y = 10
 
+
 # set the dimensions of the screen
 # and where it is placed
+root = Tk()  # create a Tk root window
+root.geometry('%dx%d+%d+%d' % (w, h+5, x, y))
+originalPlantImagelogo = PhotoImage(file="LOGOTIPO.png")
+Photologo = originalPlantImagelogo.subsample(3, 3)
+label = Label(root, image=Photologo)
+label.pack(side=TOP, expand=1, fill=X)
+root.after(5000, lambda: root.destroy())
+root.mainloop()
+root = Tk()  # create a Tk root window
+root.geometry('%dx%d+%d+%d' % (w, h+5, x, y))
+originalPlantImagelogo = PhotoImage(file="LOGOTIPO.png")
+Photologo = originalPlantImagelogo.subsample(3, 3)
+label = Label(root, image=Photologo)
+label.pack(side=TOP, expand=1, fill=X)
+labelpress = Label(root, text="PRESS TO START",font="Ostrich 8 bold")
+labelpress.pack(side=TOP, fill=BOTH, expand=1)
+root.mainloop()
+root = Tk()
 root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 topFrame = Frame(root)
 topFrame.pack(side=TOP,fill=BOTH,expand=1)
 bottomFrame = Frame(root)
 bottomFrame.pack(side=BOTTOM,fill=BOTH, expand=1)
-button1 = Button(bottomFrame, text="1", command=lambda *args: makeSomething(1))
-button1.pack(side=LEFT, fill=BOTH,expand=1)
-button2 = Button(bottomFrame, text="2", command=lambda *args: makeSomething(2))
-button2.pack(side=LEFT, fill=BOTH,expand=1)
-button3 = Button(bottomFrame, text="3", command=lambda *args: makeSomething(3))
-button3.pack(side=LEFT, fill=BOTH,expand=1)
-button4 = Button(bottomFrame, text="4", command=lambda *args: makeSomething(4))
-button4.pack(side=LEFT, fill=BOTH,expand=1)
-button5 = Button(bottomFrame, text="5", command=lambda *args: makeSomething(5))
-button5.pack(side=LEFT, fill=BOTH,expand=1)
-# img = ImageTk.PhotoImage(Image.open("duck.gif"))
-# label = Label(topFrame,image= img)
-Photo = PhotoImage(file="tux.png")
-originalPlantImage = PhotoImage(file="tux.png")
-Photo = originalPlantImage.subsample(3, 3)
+# button1 = Button(bottomFrame, text="+", command=lambda *args: makeSomething("+"))
+# button1.pack(side=LEFT, fill=BOTH,expand=1)
+# button2 = Button(bottomFrame, text="-", command=lambda *args: makeSomething("-"))
+# button2.pack(side=LEFT, fill=BOTH,expand=1)
+# button3 = Button(bottomFrame, text="next", command=lambda *args: makeSomething("des"))
+# button3.pack(side=LEFT, fill=BOTH,expand=1)
+Photo = PhotoImage(file="LEVEL.png")
+originalPlantImage = PhotoImage(file="LEVEL.png")
+Photo = originalPlantImage.subsample(5, 5)
+Photoplus = PhotoImage(file="plus.png")
+originalPlantImageplus = PhotoImage(file="plus.png")
+Photoplus = originalPlantImageplus.subsample(3, 3)
+Photominus = PhotoImage(file="minus.png")
+originalPlantImageminus = PhotoImage(file="minus.png")
+Photominus = originalPlantImageminus.subsample(3, 3)
 label = Label(topFrame, image=Photo)
 label.pack(expand=1, fill=BOTH)
+label1= Label(bottomFrame, image=Photominus)
+label2= Label(bottomFrame, text=str(nivel), font="Ostrich 40 bold")
+label3= Label(bottomFrame, image=Photoplus)
+label1.pack(side=LEFT, fill=BOTH,expand=1)
+label2.pack(side=LEFT, fill=BOTH,expand=1)
+label3.pack(side=LEFT, fill=BOTH,expand=1)
 root.mainloop()  # starts the
-print(nivel)
 #segunda forma verifica bmp
 root = Tk()
 root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-#dividimos el root en izquierda y derecha para label de metronomo izquierda para botones +/-
-leftFrame = Frame(root)
-leftFrame.pack(side=LEFT, fill=BOTH, expand=1)
-rightFrame = Frame(root)
-rightFrame.pack(side=RIGHT, fill=BOTH, expand=1)
-mycolor = '#%02x%02x%02x' % (138, 169, 206)
-label = Label(rightFrame, text=str(bpm), font="Ostrich 20 bold",fg = mycolor)
-label.pack(side=TOP, fill=BOTH, expand=1)
-button1 = Button(leftFrame, text="+", command=lambda *args:makeSomething(None, "+"))
-button1.pack(side=TOP, fill=BOTH, expand=1)
-button2 = Button(leftFrame, text="-",command=lambda *args:makeSomething(None, "-"))
-button2.pack(side=TOP, fill=BOTH, expand=1)
-root.mainloop()
+topFrame = Frame(root)
+topFrame.pack(side=TOP,fill=BOTH,expand=1)
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM,fill=BOTH, expand=1)
+# button1 = Button(bottomFrame, text="+", command=lambda *args: makeSomething("+"))
+# button1.pack(side=LEFT, fill=BOTH,expand=1)
+# button2 = Button(bottomFrame, text="-", command=lambda *args: makeSomething("-"))
+# button2.pack(side=LEFT, fill=BOTH,expand=1)
+# button3 = Button(bottomFrame, text="next", command=lambda *args: makeSomething("des"))
+# button3.pack(side=LEFT, fill=BOTH,expand=1)
+Photo = PhotoImage(file="TEMPO.png")
+originalPlantImage = PhotoImage(file="TEMPO.png")
+Photo = originalPlantImage.subsample(5, 5)
+Photoplus = PhotoImage(file="plus.png")
+originalPlantImageplus = PhotoImage(file="plus.png")
+Photoplus = originalPlantImageplus.subsample(3, 3)
+Photominus = PhotoImage(file="minus.png")
+originalPlantImageminus = PhotoImage(file="minus.png")
+Photominus = originalPlantImageminus.subsample(3, 3)
+label = Label(topFrame, image=Photo)
+label.pack(expand=1, fill=BOTH)
+label1= Label(bottomFrame, image=Photominus)
+label2= Label(bottomFrame, text=str(bpm), font="Ostrich 40 bold")
+label3= Label(bottomFrame, image=Photoplus)
+label1.pack(side=LEFT, fill=BOTH,expand=1)
+label2.pack(side=LEFT, fill=BOTH,expand=1)
+label3.pack(side=LEFT, fill=BOTH,expand=1)
+root.mainloop()  # starts the
+print(nivel)
 print(bpm)
-les=LED(5)
-les.on()
-while True:
-    pass
-
+mycolor = '#%02x%02x%02x' % (138, 169, 206)
